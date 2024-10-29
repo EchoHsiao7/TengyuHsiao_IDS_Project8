954dda88d5a910a19667d58df0c6c34253271f22
EchoHsiao7 <hsiaoty7@gmail.com>
2024-10-28 20:43:44 -0400

    1

---
+++ main.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import polars as pl
+import time
+import psutil
+
+def read_data(file):
+    df = pl.read_csv(file)
+    return df
+
+
+def generate_summary_statistics(dataframe, col_name):
+    # Mean
+    mean_value = dataframe[col_name].mean()
+    # Median
+    median_value = dataframe[col_name].median()
+    # Standard deviation
+    std_value = dataframe[col_name].std()
+
+    print(f"Summary Statistics for {col_name}:")
+    print(f"Mean: {mean_value}")
+    print(f"Median: {median_value}")
+    print(f"Standard Deviation: {std_value}")
+
+    return mean_value, median_value, std_value
+
+
+if __name__ == "__main__":
+    process = psutil.Process()
+    cpu_start = process.cpu_percent(interval=None)
+    start_time = time.time()
+    input_file = "Steam_2024_bestRevenue_1500.csv"
+    df_data = read_data(input_file)
+    colname = "price"  # Replace with the actual column name
+    mean, median, std = generate_summary_statistics(df_data, colname)
+    end_time = time.time()
+    cpu_end = process.cpu_percent(interval=None)
+    print("CPU Usage (%):", cpu_end)
+    print("Execution time:", end_time - start_time)
--- main/src/add.rs
+++ main/src/add.rs
@@ -1,8 +1,39 @@
-use add::add;
+use polars::prelude::*;
+use std::error::Error;
+use plotters::prelude::*;
+use std::time::Instant;
 
-fn main() {
-    let a = 2;
-    let b = 3;
-    let result = add(&a, &b);
-    println!("Adding {} and {} is {}", a, b, result);
+fn read_data(file: &str) -> Result<DataFrame, Box<dyn Error>> {
+    let df = CsvReader::from_path(file)?.infer_schema(Some(100)).finish()?;
+    Ok(df)
+}
+
+fn generate_summary_statistics(df: &DataFrame, col_name: &str) -> Result<(f64, f64, f64), Box<dyn Error>> {
+    let column = df.column(col_name)?;
+
+    let mean_value = column.mean();
+    let median_value = column.median();
+    let std_value = column.std();
+
+    println!("Summary Statistics for {}:", col_name);
+    println!("Mean: {:?}", mean_value);
+    println!("Median: {:?}", median_value);
+    println!("Standard Deviation: {:?}", std_value);
+
+    Ok((mean_value.unwrap(), median_value.unwrap(), std_value.unwrap()))
+}
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let start = Instant::now();
+    let input_file = "Steam_2024_bestRevenue_1500.csv";
+    let col_name = "price"; 
+
+    let df = read_data(input_file)?;
+    let column = df.column(col_name)?.f64()?.into_no_null_iter().collect::<Vec<f64>>();
+    let (mean, median, std) = generate_summary_statistics(&df, col_name)?;
+    let duration = start.elapsed();
+    println!("Execution time: {:?}", duration);
+
+    Ok(())
 }
